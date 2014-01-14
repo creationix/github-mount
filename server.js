42c400a0a078312570bb2eda987ffc6e5a24f629
Tim Caswell <tim@creationix.com>
2014-01-13 21:05:27 -0800

    In progress refactor of filter compiler

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var vm = require('vm');
 var handleRequest = require('git-publish-http');
 var gitPublisher = require('git-publisher');
 var http = require('http');
+var mine = require('js-linker/mine.js');
+var pathJoin = require('js-linker/pathjoin.js');
+var parallel = require('js-git/lib/parallel.js');
 
 var repos = {};
 
 var server = http.createServer(onRequest);
-server.listen(process.env.PORT || 8000, function () {
+server.listen(process.env.PORT || 8080, function () {
   console.log("Server listening at http://localhost:%s/", server.address().port);
 });
 
@@ -23,9 +26,10 @@ function onRequest(req, res) {
   var repo = repos[host];
   if (!repo) {
     repo = repos[host] = jsGithub("creationix/" + host, accessToken);
-    gitPublisher(repo, compileModule);
-
-    repo.getRoot = rootCheck(repo);
+    repo.handleCommand = handleCommand;
+    gitPublisher(repo);
+    repo.getRoot = rootCheck(repo, onRequest.bind(this, req, res));
+    return;
   }
 
   // Log requests
@@ -37,6 +41,121 @@ function onRequest(req, res) {
 
   handleRequest(repo, repo.getRoot(), req, res);
 }
+
+function handleCommand(req, callback) {
+  var repo = req.repo;
+  var root = req.root;
+  var name = req.name;
+
+  return compile(repo, root, "filters/" + name + ".js", function (err, result) {
+    if (err) return callback(err);
+    console.log("RESULT", result);
+  });
+  
+//   function onEntry(err, entry) {
+//     if (err) return callback(err);
+//     if (!entry) return new Error("Unknown filter: " + name);
+//     module.hash = entry.hash;
+//     var deps = modDeps[entry.hash];
+//     if (deps) {
+//       module.deps = deps;
+//       return onDeps();
+//     }
+//     repo.loadAs("text", entry.hash, onJs);
+//   }
+  
+//   function onJs(err, js) {
+//     if (err) return callback(err);
+//     var deps = module.deps = mine(js);
+//     module.js = js;
+//     parallel(deps.map(function (match) {
+//       return repo.pathToEntry(root, "filters/modules/" + match.name + ".js");
+//     }), onEntries);
+//   }
+  
+//   function onEntries(err, entries) {
+//     if (err) return callback(err);
+//     var deps = module.deps;
+//     entries.forEach(function (entry, i) {
+//       deps[i].hash = entry.hash;
+//     });
+//     onDeps();
+//   }
+  
+//   function onDeps() {
+//     console.log(module);
+//   }
+      
+//   var top = cache[root];
+//   var dir = top.filters;
+//   if (!dir) return callback(new Error("Missing filters in root: " + root));
+//   var tree = cache[dir.hash];
+//   if (!tree) {
+//     return repo.loadAs("tree", dir.hash, function (err, tree) {
+//       if (err) return callback(err);
+//       cache[dir.hash] = tree;
+//       return handleCommand(req, callback);
+//     });
+//   }
+//   var entry = tree[name + ".js"];
+//   if (!entry) {
+//     return callback(new Error("No such filter '" + req.name + "' in root: " + root));
+//   }
+//   var module = modules[name];
+  
+//   function onModule() {
+//   if (!module) {
+//     return repo.loadAs("text", entry.hash, function (err, js) {
+//       if (err) return callback(err);
+//       modules[name] = {
+//         hash: entry.hash,
+//         fn: compileModule(js, "git:" + root + ":/filters/" + name + ".js")
+//       };
+//     return handleCommand(req, callback);
+//     });
+//   }
+//   module.fn(req, callback);
+}
+
+// Cached modules by path
+// module contains { hash, deps: [{path,hash}], fn, pending:[callback] }
+var defs = {};
+function compile(repo, root, path, callback) {
+  var def = {
+    hash: null,
+    deps: null,
+    fn: null,
+  };
+  return repo.pathToEntry(root, path, onEntry);
+  
+  function onEntry(err, entry) {
+    if (!entry) return callback(err);
+    def.hash = entry.hash;
+    return repo.loadAs("text", entry.hash, onJs);
+  }
+  
+  function onJs(err, js) {
+    if (err) return callback(err);
+    var deps = def.deps = [];
+    parallel(mine(js).map(function (dep, i) {
+      var depPath = pathJoin(path, "..", dep.name);
+      deps[i] = { path: depPath, hash: null };
+      return repo.pathToEntry(root, depPath);
+    }), onEntries);
+  }
+  
+  function onEntries(err, entries) {
+    if (err) return callback(err);
+    var deps = def.deps;
+    entries.forEach(function (entry, i) {
+      deps[i].hash = entry.hash;
+    });
+    console.log(def)
+  }
+}
+
+
+
 
 function compileModule(js, filename) {
   var exports = {};
@@ -65,7 +184,7 @@ function fakeRequire(name) {
 }
 
 
-function rootCheck(repo) {
+function rootCheck(repo, callback) {
   // Get the root, but throttle request rate.
   var root;
   var last = Date.now();
@@ -84,5 +203,6 @@ function rootCheck(repo) {
   function onRoot(err, commit) {
     if (err) console.error(err.stack);
     if (commit) root = commit.tree;
+    callback && callback(err);
   }
 }
